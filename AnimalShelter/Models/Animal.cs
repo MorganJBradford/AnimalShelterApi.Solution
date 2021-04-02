eb4a6601b3a6b5f7d9f0f2995168439a221e230d
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-02 13:50:50 -0500

    remove enum; change enum types to strings. Add regex

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -8,22 +8,10 @@ namespace AnimalShelter.Models
     [Required]
     public string Name { get; set; }
     [Required]
-    public Species Species { get; set; }
+    public string Species { get; set; }
     public int Age { get; set; }
     [Required]
-    // [RegularExpression("['Female'|'female'|'Male'|'male']")]
-    public Gender Gender { get; set; }
-  }
-
-  public enum Species
-  {
-    Cat,
-    Dog
-  }
-
-  public enum Gender
-  {
-    Male,
-    Female
+    [RegularExpression("['Female'|'female'|'Male'|'male']")]
+    public string Gender { get; set; }
   }
 }

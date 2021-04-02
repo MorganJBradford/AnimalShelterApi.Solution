da93c1b64226d859043ae3baf97ed63ad6ff3f91
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-02 13:51:46 -0500

    add test seed data

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -8,6 +8,18 @@ namespace AnimalShelter.Models
       : base(options)
       {
       }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+        .HasData(
+          new Animal { AnimalId = 1, Name = "Gizmo", Species = "Cat", Age = 7, Gender = "Female" },
+          new Animal { AnimalId = 2, Name = "Gerry", Species = "Doge", Age = 7, Gender = "Male" },
+          new Animal { AnimalId = 3, Name = "Geraldo", Species = "Doge", Age = 7, Gender = "Jerry" },
+          new Animal { AnimalId = 4, Name = "Guillermo", Species = "Cat", Age = 7, Gender = "Terry" },
+          new Animal { AnimalId = 5, Name = "Gizmo", Species = "Cat", Age = 7, Gender = "Berry" }
+        );
+    }
     public DbSet<Animal> Animals { get; set; }
   }
 }

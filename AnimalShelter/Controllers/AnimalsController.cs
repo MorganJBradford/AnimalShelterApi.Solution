3aa0abb2266453d737d96a1935b9f3850d71acca
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-02 12:41:04 -0500

    add queryable [Get]Get route; params = name, species, gender, age

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -1,5 +1,9 @@
 using AnimalShelter.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 
 namespace AnimalShelter.Controllers
@@ -12,6 +16,35 @@ namespace AnimalShelter.Controllers
     public AnimalsController(AnimalShelterContext db)
     {
       _db = db;
+    }
+
+    // GET: api/Animals
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Animal>>> Get(string name, string species, string gender, int age)
+    {
+      var query = _db.Animals.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(e => e.Name == name);
+      }
+
+      if (species != null)
+      {
+        query = query.Where(e => e.Species.ToString() == species);
+      }
+
+      if (gender != null)
+      {
+        query = query.Where(e => e.Gender.ToString() == gender);
+      }
+
+      if (age != 0)
+      {
+        query = query.Where(e => e.Age == age);
+      }
+
+      return await query.ToListAsync();
     }
   }
 }

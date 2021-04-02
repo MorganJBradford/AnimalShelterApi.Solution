317a7ca1ff0b8a8259c6519b2211e28ec6334245
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-02 11:42:12 -0500

    setup context file; connext animals to db

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -4,6 +4,10 @@ namespace AnimalShelter.Models
 {
   public class AnimalShelterContext : DbContext
   {
-    
+    public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+      : base(options)
+      {
+      }
+    public DbSet<Animal> Animals { get; set; }
   }
 }

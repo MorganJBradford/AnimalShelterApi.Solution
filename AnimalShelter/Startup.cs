43a88125dc788db38e1d6ae3c8f5f128eaf4aec4
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-03 18:36:06 -0500

    connect JwtConfig class to appsettings.json; add configure JwtBearer settings

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -1,12 +1,16 @@
+using AnimalShelter.Configuration;
 using AnimalShelter.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Text;
 
 namespace AnimalShelter
 {
@@ -22,8 +26,33 @@ namespace AnimalShelter
     public void ConfigureServices(IServiceCollection services)
     {
 
+      services.Configure<JwtConfig>(Configuration.GetSection("JwtConfig"));
+
       services.AddDbContext<AnimalShelterContext>(opt =>
         opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+
+      services.AddAuthentication(options =>
+      {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+      })
+      .AddJwtBearer(jwt =>
+      {
+        var key = Encoding.ASCII.GetBytes(Configuration["JwtConfig"]);
+
+        jwt.SaveToken = true;
+        jwt.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidateIssuerSigningKey = true,
+          IssuerSigningKey = new SymmetricSecurityKey(key),
+          ValidateIssuer = false,
+          ValidateAudience = false,
+          ValidateLifetime = true,
+          RequireExpirationTime = false,
+        };
+      });
+
       services.AddControllers();
       services.AddSwaggerGen(c =>
       {

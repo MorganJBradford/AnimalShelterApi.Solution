b95ef195b96643c7af6f363a9f23cb3d9e825fd2
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-03 20:30:36 -0500

    add constructor; pass userManager and jwtConfig into constructor

--- AnimalShelter/Controllers/AuthManagementController.cs
+++ AnimalShelter/Controllers/AuthManagementController.cs
@@ -1,6 +1,11 @@
 using AnimalShelter.Configuration;
+using AnimalShelter.Models.DTOs.Requests;
+using AnimalShelter.Models.DTOs.Responses;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AnimalShelter.Controllers
 {
@@ -10,5 +15,11 @@ namespace AnimalShelter.Controllers
   {
     private readonly UserManager<IdentityUser> _userManager;
     private readonly JwtConfig _jwtConfig;
+
+    public AuthManagementController(UserManager<IdentityUser> userManager, IOptionsMonitor<JwtConfig> optionsMonitor)
+    {
+      _userManager = userManager;
+      _jwtConfig = optionsMonitor.CurrentValue;
+    }
   }
 }

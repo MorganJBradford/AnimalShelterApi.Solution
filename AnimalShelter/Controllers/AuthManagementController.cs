a3d1f758e84db73be443f5cf4727549b8af7d3aa
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-03 20:37:53 -0500

    WIP: add [Post]Register route. Add exisisting user error handling; add new usuer functionality.

--- AnimalShelter/Controllers/AuthManagementController.cs
+++ AnimalShelter/Controllers/AuthManagementController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AnimalShelter.Controllers
@@ -21,5 +22,54 @@ namespace AnimalShelter.Controllers
       _userManager = userManager;
       _jwtConfig = optionsMonitor.CurrentValue;
     }
+
+    [HttpPost]
+    [Route("Register")]
+    public async Task<IActionResult> Register([FromBody] UserRegistrationDto user)
+    {
+      if (ModelState.IsValid)
+      {
+        var existingUser = await _userManager.FindByEmailAsync(user.Email);
+
+        if(existingUser != null)
+        {
+          return BadRequest(new RegistrationResponse()
+          {
+            Errors = new List<string>()
+            {
+              "Email already in use"
+            },
+            Success = false
+          });
+        }
+
+        var newUser = new IdentityUser() { Email = user.Email, UserName = user.Email };
+        var isCreated = await _userManager.CreateAsync(newUser, user.Password);
+
+        if(isCreated.Succeeded)
+        {
+
+        }
+        else
+        {
+          return BadRequest(new RegistrationResponse()
+          {
+            Errors = isCreated.Errors.Select(x => x.Description).ToList(),            
+            Success = false
+          });
+        }
+      }
+
+      return BadRequest(new RegistrationResponse()
+      {
+        Errors = new List<string>()
+        {
+          "Invalid payload"
+        },
+        Success = false
+      });
+    }
+
+
   }
 }

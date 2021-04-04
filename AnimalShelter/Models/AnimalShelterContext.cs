ce6eab1d1b2202d45ffcb124a76ca93052a6ff98
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-03 21:37:51 -0500

    return seed data to migrations

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -9,6 +9,19 @@ namespace AnimalShelter.Models
       : base(options)
       {
       }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+        .HasData(
+          new Animal { AnimalId = 1, Name = "Gizmo", Species = "Cat", Age = 7, Gender = "F" },
+          new Animal { AnimalId = 2, Name = "Gerry", Species = "Dog", Age = 7, Gender = "M" },
+          new Animal { AnimalId = 3, Name = "Geraldo", Species = "Dog", Age = 7, Gender = "M" },
+          new Animal { AnimalId = 4, Name = "Guillermo", Species = "Cat", Age = 7, Gender = "F" },
+          new Animal { AnimalId = 5, Name = "Gizmo", Species = "Cat", Age = 7, Gender = "F" }
+        );
+      base.OnModelCreating(builder);
+    }
     public DbSet<Animal> Animals { get; set; }
   }
 }
